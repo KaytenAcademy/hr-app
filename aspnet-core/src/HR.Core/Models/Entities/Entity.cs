4da414ef58e8103d2c6a1d099ba4fdfa811db8ea
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-01-08 11:46:20 +0300

    Create Entity.cs
    Entity oluşturuldu

---
+++ aspnet-core/src/HR.Core/Models/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.Models.Entities
+{
+    public class Entity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
