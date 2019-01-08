792a8426e37755eb47e0585881ed639d3bcf4b27
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-01-08 11:45:51 +0300

    Create Employee.cs
    Eployee oluşturuldu

---
+++ aspnet-core/src/HR.Core/Models/Entities/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.Models.Entities
+{
+    class Employee :Entity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public virtual IEnumerable<EmployeeDepartment> EmployeeDepartments { get; set; }
+
+        public void Hire(DateTime Start, DateTime End, string Role)
+        {
+        }
+        public void Fire(int EmployeeId)
+        {
+        }
+    }
+}
