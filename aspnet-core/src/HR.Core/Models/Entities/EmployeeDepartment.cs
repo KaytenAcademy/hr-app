c87c4a98f82049885a9bac4f5869ff419ae7b1ea
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-01-08 11:46:08 +0300

    Create EmployeeDepartment.cs
    EmployeeDepartment oluşturuldu

---
+++ aspnet-core/src/HR.Core/Models/Entities/EmployeeDepartment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.Models.Entities
+{
+    class EmployeeDepartment :Entity
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndTime { get; set; }
+        public string  Role { get; set; }
+
+        public Employee Employee { get; set; }
+        public Department Department { get; set; }
+    }
+}
