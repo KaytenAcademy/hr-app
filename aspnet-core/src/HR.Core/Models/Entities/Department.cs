09b27cd855745ddfab8d8dde22ebdaa43a584afb
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-01-08 11:45:33 +0300

    Create Department.cs
    Department oluşturuldu

---
+++ aspnet-core/src/HR.Core/Models/Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HR.Models.Entities
+{
+    class Department:Entity
+    {
+        public string Name { get; set; }
+
+        public Company Company { get; set; }
+        public virtual IEnumerable<EmployeeDepartment> EmployeeDeparments { get; set; }
+    }
+}
