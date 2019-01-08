f5f59e0fbfba04351dfb954c11cdd49779d55dee
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-01-08 11:45:09 +0300

    Create Company.cs
    Company oluşturuldu

---
+++ aspnet-core/src/HR.Core/Models/Entities/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.Models.Entities
+{
+    class Company:Entity
+    {
+        public string Name { get; set; }
+        public virtual IEnumerable<Department> Deparments { get; set; }
+    }
+}
